de1770089d58a811def9223d23731760aa3e0898
AG72 <acuario7203@gmail.com>
2025-07-11 20:47:00 -0400

    FEAT[SCRUM-0] : Rutas_Gateway

---
+++ Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .SetIsOriginAllowed(origin => true)
+            .AllowCredentials();
+
+    });
+});
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.UseCors("AllowFrontend");
+app.MapReverseProxy();
+
+app.Run();
+
